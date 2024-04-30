68dc353aaa2fbd7de55e6393035f91f11b2aa765
kimchieater <jasonj19276@gmail.com>
2024-04-30 19:14:37 +1000

    helloooo

---
+++ personal-blog-app/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+
+export default function Home() {
+  return (
+    <>
+    <h1>hello</h1>
+    <Button variant={"outline"}>Click me</Button>
+    </>
+  );
+}
